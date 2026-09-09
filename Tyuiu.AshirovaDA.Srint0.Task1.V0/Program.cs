62d4beec0729e4e7319cd989a114a1350b772f89
Диана Аширова <oiseuk@mail.ru>
2026-09-09 20:03:49 +0500

    Добавили Task1

---
+++ Tyuiu.AshirovaDA.Srint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AshirovaDA.Srint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Диана";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+
+        }
+    }
+}
